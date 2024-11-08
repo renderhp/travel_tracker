0d08002eb81926040f813d7bf9587fcddbc8457f
Vossie84 <andriesjnr@wofb.co.za>
2024-11-08 13:15:15 +0000

    - Added Last Action - Added Online Status - Added Player Level - Added Attack Button

--- src/components/TravelTable.js
+++ src/components/TravelTable.js
@@ -7,13 +7,13 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Stack, Collapse, IconButton, Typography } from '@mui/material';
+import {Stack, Collapse, IconButton, Typography} from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import SentimentDissatisfiedIcon from '@mui/icons-material/SentimentDissatisfied'; // Import an icon for a better look
 import TimeAgo from 'react-timeago';
 
 
-export default function TravelTable({ countryName, playersInCountry }) {
+export default function TravelTable({countryName, playersInCountry}) {
     // console.log(playersInCountry)
     const [open, setOpen] = React.useState(true); // State to manage the collapse
     const getColor = (color) => {
@@ -26,25 +26,52 @@ export default function TravelTable({ countryName, playersInCountry }) {
         }
         return color
     }
+
+    const getStatusColor = (status) => {
+        if (status === "Offline") {
+            return "#8e9394"
+        } else if (status === "Idle") {
+            return "#EEA900"
+        } else if (status === "Online") {
+            return "#99CC00"
+        }
+        return status
+    }
+
     const table = (
         <TableContainer component={Paper}>
             <Table aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell align="left" sx={{ width: '100px' }}>ID</TableCell>
-                        <TableCell align="left" sx={{ width: '100px' }}>Name</TableCell>
-                        <TableCell align="center" sx={{ flexGrow: 1 }}>Status</TableCell>
-                        <TableCell align="right" sx={{ width: '100px' }}>Last Change</TableCell>
+                        <TableCell align="left" sx={{width: '5%'}}>Action</TableCell>
+                        <TableCell align="left" sx={{width: '5%'}}>ID</TableCell>
+                        <TableCell align="left" sx={{width: '15%'}}>Name (Lvl)</TableCell>
+                        <TableCell align="center" sx={{flexGrow: 1}}>Travel Status</TableCell>
+                        <TableCell align="center" sx={{flexGrow: 1}}>Online Status</TableCell>
+                        <TableCell align="left" sx={{width: '10%'}}>Last Change</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {playersInCountry.map((row) => (
+
                         <TableRow
                             key={row.id}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            sx={{'&:last-child td, &:last-child th': {border: 0}}}
                         >
-                            <TableCell align="left" sx={{ width: '100px' }}>{row.id}</TableCell>
-                            <TableCell align="left" sx={{ width: '100px' }}>
+                            <TableCell align="left" sx={{width: '100px'}}>
+                                <Typography
+                                    component="a"
+                                    href={`https://www.torn.com/loader.php?sid=attack&user2ID=${row.id}`} // Change this to your desired URL
+                                    target="_blank"
+                                    className="button"
+                                    rel="noopener noreferrer"
+
+                                >
+                                    Attack
+                                </Typography>
+                            </TableCell>
+                            <TableCell align="left" sx={{width: '100px'}}>{row.id}</TableCell>
+                            <TableCell align="left" sx={{width: '100px'}}>
                                 <Typography
                                     component="a"
                                     href={`https://www.torn.com/profiles.php?XID=${row.id}`} // Change this to your desired URL
@@ -58,13 +85,16 @@ export default function TravelTable({ countryName, playersInCountry }) {
                                         },
                                     }}
                                 >
-                                    {row.name}
+                                    {row.name} <br/> ({row.level})
                                 </Typography>
                             </TableCell>
-                            <TableCell align="center" sx={{ flexGrow: 1, color: getColor(row.color) }}>
+                            <TableCell align="center" sx={{flexGrow: 1, color: getColor(row.color)}}>
                                 {row.status}
                             </TableCell>
-                            <TableCell align="right" sx={{ width: '100px' }}>{row.last_status_change > 0 ? <TimeAgo date={row.last_status_change} /> : "N/A"}</TableCell>
+                            <TableCell align="center" >
+                                <span style={{ color: getStatusColor(row.online_status) }}> {row.online_status}  <br/> {row.last_action} </span>
+                            </TableCell>
+                            <TableCell align="left" >{row.last_status_change > 0 ? <TimeAgo date={row.last_status_change}/> : "N/A"}</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
@@ -81,7 +111,7 @@ export default function TravelTable({ countryName, playersInCountry }) {
             <Stack spacing={1}>
                 <h3>
                     <IconButton onClick={() => setOpen(!open)}>
-                        <ExpandMoreIcon />
+                        <ExpandMoreIcon/>
                     </IconButton>
                     {countryName}
                 </h3>
@@ -89,8 +119,8 @@ export default function TravelTable({ countryName, playersInCountry }) {
                     {playersInCountry.length > 0
                         ? table
                         : (
-                            <Box display="flex" alignItems="center" justifyContent="center" sx={{ padding: 1 }}>
-                                <SentimentDissatisfiedIcon fontSize="large" sx={{ marginRight: 1 }} />
+                            <Box display="flex" alignItems="center" justifyContent="center" sx={{padding: 1}}>
+                                <SentimentDissatisfiedIcon fontSize="large" sx={{marginRight: 1}}/>
                                 <Typography variant="h6" color="text.secondary">
                                     Nobody's here
                                 </Typography>
